a7532c839983c29b9b26f3ea70bd1be4cb6dd456
josejosemarcosribeiro-alt <josejosemarcosribeiro@gmail.com>
2025-12-03 15:32:10 -0500

    Add routes for list item management
    Fix frontend upload issue: added backend support for video uploads
    
    - Configured multer to handle video uploads from add_list.pug form
    - Set storage to local 'uploads/' folder and saved file path in MongoDB
    - Updated /list/add POST route to receive video file with req.file
    - Ensured existing fields (title, body) continue to save correctly
    - This fixes the "Internal Server Error" caused by form sending multipart/form-data without backend handling
    - Prepares the system for future integration with Amazon S3 or other external storage

---
+++ routes/list.js
@@ -0,0 +1,126 @@
+const express = require('express');
+const router = express.Router();
+const multer = require('multer');
+
+// bring in models
+let list_item = require('../models/listitem');
+let User = require('../models/user');
+
+// Configuração do multer (upload local)
+const storage = multer.diskStorage({
+    destination: function(req, file, cb){
+        cb(null, 'uploads/'); // pasta onde os vídeos serão salvos
+    },
+    filename: function(req, file, cb){
+        cb(null, Date.now() + '-' + file.originalname);
+    }
+});
+const upload = multer({ storage: storage });
+
+// edit item route
+router.get('/item/edit/:id', ensureAuthenticated, function(req, res){
+    list_item.findById(req.params.id, function(err, item){
+        if(item.author != req.user._id){
+            req.flash('danger','Not authenticated');
+            res.redirect('/');
+        }
+        res.render('edit_item', { item: item });
+    });
+});
+
+// get item route
+router.get('/item/:id', function(req, res){
+    list_item.findById(req.params.id, function(err, item){
+        if(err){
+            console.log(err);
+        } else {
+            User.findById(item.author, function(err, user){
+                res.render('item', {
+                    item: item,
+                    author: user.name
+                });
+            });
+        }
+    });
+});
+
+// add route (com upload de vídeo)
+router.get('/add', ensureAuthenticated, function(req,res){
+    res.render('add_list');
+});
+
+router.post('/add', ensureAuthenticated, upload.single('video'), function(req,res){
+    req.checkBody('title','Title is required').notEmpty();
+    req.checkBody('body','Body is required').notEmpty();
+
+    let errors = req.validationErrors();
+    if(errors){
+        res.render('add_list', { errors: errors });
+    } else {
+        let item = new list_item();
+        item.title = req.body.title;
+        item.author = req.user._id;
+        item.body = req.body.body;
+
+        if(req.file){
+            item.videoPath = req.file.path; // salvar caminho do vídeo no MongoDB
+        }
+
+        item.save(function(err){
+            if(err){
+                console.log(err);
+                return;
+            } else {
+                req.flash('success','Listing Added!');
+                res.redirect('/');
+            }
+        });
+    }
+});
+
+// edit item route
+router.post('/edit/:id', ensureAuthenticated, function(req,res){
+    let item = {};
+    item.title = req.body.title;
+    item.author = req.user._id;
+    item.body = req.body.body;
+
+    let query = {_id: req.params.id};
+    list_item.updateOne(query, item, function(err){
+        if(err){
+            console.log(err);
+            return;
+        } else {
+            req.flash('success','Listing Edited!');
+            res.redirect('/');
+        }
+    });
+});
+
+router.delete('/item/:id', function(req,res){
+    let query = {_id:req.params.id};
+    if(!req.user._id){
+        res.status(500).send();
+    }
+    list_item.findById(req.params.id, function(err, item){
+        if(item.author != req.user._id){
+            res.status(500).send();
+        } else {
+            list_item.remove(query, function(err){
+                if(err) console.log(err);
+                res.send('Success');
+            });
+        }
+    });
+});
+
+function ensureAuthenticated(req,res,next){
+    if(req.isAuthenticated()){
+        return next();
+    } else {
+        req.flash('danger','Please login');
+        res.redirect('/users/login');
+    }
+}
+
+module.exports = router;
